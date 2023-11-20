c4a3d09eff24fafa3925cc89f433940a75f57c9f
Maria Victória Fernandes Vaz <94924671+mariavictoriafv@users.noreply.github.com>
2023-11-20 13:20:29 -0300

    Update Querys_Refera.sql

--- Querys_Refera.sql
+++ Querys_Refera.sql
@@ -15,7 +15,7 @@ JOIN
 GROUP BY
     P.ProdutoID
 ORDER BY
-	YEAR(STR_TO_DATE(V.Data, '%d/%m/%Y')),
+    YEAR(STR_TO_DATE(V.Data, '%d/%m/%Y')),
     MONTH(STR_TO_DATE(V.Data, '%d/%m/%Y')),
     DAY(STR_TO_DATE(V.Data, '%d/%m/%Y'));
 
@@ -26,9 +26,9 @@ SELECT
     P.CupomID,
     P.Valor As Valor_De_Venda
 FROM
-	produtos P
+    produtos P
 ORDER BY
-	P.ProdutoID;
+    P.ProdutoID;
 
 /* Consulta 3: Quantidade e valor das vendas por dia, mês, ano */
 SELECT
@@ -50,15 +50,15 @@ ORDER BY
     Coloquei também o ano de cada mês 
     para que o resultado faça mais sentido */
 SELECT 
-	MONTH(STR_TO_DATE(V.Data, '%d/%m/%Y')) AS Mes,
+    MONTH(STR_TO_DATE(V.Data, '%d/%m/%Y')) AS Mes,
     YEAR(STR_TO_DATE(V.Data, '%d/%m/%Y')) AS Ano,
     SUM(P.ValorLiquido) AS Lucro_Do_Mes
 FROM 
-	Vendas V
+    Vendas V
 JOIN 
-	Produtos P ON V.CupomID = P.CupomID
+    Produtos P ON V.CupomID = P.CupomID
 GROUP BY
-	Mes,
+    Mes,
     Ano
 ORDER BY
     Ano,
@@ -66,32 +66,32 @@ ORDER BY
 
 /* Consulta 5: Venda por produto */
 SELECT
-	P.ProdutoID,
+    P.ProdutoID,
     COUNT(*) AS Total_De_Vendas 	
 FROM
-	produtos P
+    produtos P
 JOIN
-	vendas V ON V.CupomID = P.CupomID
+    vendas V ON V.CupomID = P.CupomID
 GROUP BY
-	P.ProdutoID
+    P.ProdutoID
 ORDER BY
-	P.ProdutoID;
+    P.ProdutoID;
 
 /* Consulta 6: Venda por cliente, cidade do cliente e estado
     Como não há o estado do cliente na tabelas, coloquei o 
     país no lugar */
 SELECT 
-	C.Cliente, 
+    C.Cliente, 
     C.NomeContato, 
     C.Cidade, 
     C.Pais AS Estado_Pais,
     COUNT(*) AS Vendas
 FROM
-	clientes C
+    clientes C
 JOIN
-	vendas V ON C.ClienteID = V.ClienteID
+    vendas V ON C.ClienteID = V.ClienteID
 GROUP BY
-	C.ClienteID;
+    C.ClienteID;
 
 /* Consulta 7: Média de produtos vendidos */
 SELECT
